9a13b91c2483ae535a994bff0855d1ff710f0b61
Milan-Sharma-449 <milansharma449@gmail.com>
2024-01-07 17:15:54 +0530

    added graphbase

---
+++ grafbase.config.ts
@@ -0,0 +1,56 @@
+import { g, config, auth } from '@grafbase/sdk';
+
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
+})
+
+// @ts-ignore
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret:  g.env('NEXTAUTH_SECRET')
+})
+
+export default config({
+  graph: g,
+  // Authentication - https://grafbase.com/docs/auth
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public()
+    },
+  },
+  // Caching - https://grafbase.com/docs/graphql-edge-caching
+  // cache: {
+  //   rules: [
+  //     {
+  //       types: ['Query'], // Cache everything for 60 seconds
+  //       maxAge: 60,
+  //       staleWhileRevalidate: 60
+  //     }
+  //   ]
+  // }
+})
